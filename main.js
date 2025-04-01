f93b458219ddddb3f0ff7998b009a5d06c4de170
Kenjie Tarasona <148972543+Lain-lies@users.noreply.github.com>
2025-04-01 14:10:10 +0800

    Add initial findPath() implementation

--- main.js
+++ main.js
@@ -1,43 +1,32 @@
 class Path {
-  constructor(x, y) {
+  constructor(position, prev = null) {
     this.prev = null;
-    this.currentPosition = [x, y];
-    this.children = [];
+    this.position = position;
   }
 }
-const limit = 7;
-const visited = new Set();
 
-// function findPath(path, end){
-
-//     let [xStart , yStart] = path.currentPosition;
-//     let [xEnd, yEnd] = end;
-
-//     const visited = new Set();
-
-// }
+const graph = {
+  limit: 7,
+  visited: new Set(),
+};
 
 function traverse(currentPos) {
   let [xPos, yPos] = currentPos;
+
+  let temp = [];
 
   // Horizontal
   for (let i = -2; i <= 2; i += 4) {
     let xTempPos, yTempPos;
     xTempPos = xPos + i;
-    if (xTempPos < 0 || xTempPos > limit) {
-      console.log(1);
-      continue;
-    }
+    if (xTempPos < 0 || xTempPos > graph.limit) continue;
 
     for (let j = -1; j <= 1; j += 2) {
       yTempPos = yPos + j;
-      console.log(yTempPos);
 
-      if (yTempPos < 0 || yTempPos > limit) {
-        console.log(2);
-        continue;
-      }
-      visited.add(`[${xTempPos}, ${yTempPos}]`);
+      if (yTempPos < 0 || yTempPos > graph.limit) continue;
+
+      temp.push(`[${xTempPos}, ${yTempPos}]`);
     }
   }
 
@@ -45,31 +34,44 @@ function traverse(currentPos) {
   for (let i = -1; i <= 1; i += 2) {
     let xTempPos, yTempPos;
     xTempPos = xPos + i;
-    if (xTempPos < 0 || xTempPos > limit) continue;
+    if (xTempPos < 0 || xTempPos > graph.limit) continue;
 
     for (let j = -2; j <= 2; j += 4) {
       yTempPos = yPos + j;
-      if (yTempPos < 0 || yTempPos > limit) continue;
-      visited.add(`[${xTempPos}, ${yTempPos}]`);
+      if (yTempPos < 0 || yTempPos > graph.limit) continue;
+      temp.push(`[${xTempPos}, ${yTempPos}]`);
+    }
+  }
+
+  temp = temp.filter((element) =>
+    graph.visited.has(`${element}`) ? false : true
+  );
+
+  temp.forEach((element) => graph.visited.add(`${element}`));
+
+  return temp;
+}
+
+function checkEnd(paths) {
+  if (paths.includes(graph.end)) return true;
+
+  return false;
+}
+
+function findPath(start, end) {
+  const startingPath = new Path(start);
+  graph.end = `[${end[0]}, ${end[1]}]`;
+
+  const queue = [startingPath];
+
+  while (queue.length !== 0) {
+    const current = queue[0];
+
+    const paths = traverse(current.position);
+
+    if (checkEnd(paths)) {
     }
   }
 }
 
-traverse([0, 0]);
-console.log(visited);
-console.log(visited.keys());
-
-/*
-
-    xPosition - 2 , yPosition - 1
-    xPosition - 2 , yPosition + 1
-    xPosition + 2 , yPosition - 1 
-    xPosition + 2 , yPosition + 1
-
-    xPosition - 1 , yPosition + 2
-    xPosition - 1,  yPosition - 2
-    xPosition + 1 , yPosition + 2
-    xPosition + 1 , yPosition - 2
-
-
-*/
+findPath([0, 0], [1, 2]);
